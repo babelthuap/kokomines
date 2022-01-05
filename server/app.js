5a187bcbc61824cbd2ae36f1a65a5d5fa142b211
Nicholas Neumann-Chun <babelthuap@users.noreply.github.com>
2022-01-05 14:06:09 -0800

    Split socket code into its own file

--- server/app.js
+++ server/app.js
@@ -12,67 +12,13 @@ const {Server} = require('socket.io');
 const io = new Server(server);
 
 // My modules
-const {getPublicState, restart, handleClick} = require('./minesweeper.js');
-const {log, time} = require('./logger.js');
+const {handleConnection} = require('./socket.js');
+const {log} = require('./logger.js');
 
 // Initialize app
 app.use(express.static('client'));
-io.on('connection', handleConnection);
+io.on('connection', (socket) => handleConnection(socket, io));
 server.listen(PORT, handleServerStartup);
-
-// Handles communication with a socket
-const hovering = {};
-const recentlyClicked = new Map();
-function handleConnection(socket) {
-  log(`user ${socket.id} connected`);
-
-  socket.emit('init', getPublicState());
-
-  socket.on('hover', (i) => {
-    if (i !== null) {
-      hovering[socket.id] = i;
-    } else {
-      delete hovering[socket.id];
-    }
-    socket.broadcast.emit('hover', hovering);
-  });
-
-  socket.on('click', ([i, rightClick]) => {
-    const state = getPublicState();
-    if (state.gameInProgress) {
-      // Avoid conflicting clicks from different clients
-      const clicked = recentlyClicked.get(i);
-      if (clicked) {
-        if (clicked.socketId !== socket.id) {
-          // Notify this client that the update failed
-          return socket.emit('update', {tiles: [[[i, state.board.tiles[i]]]]});
-        } else {
-          clearTimeout(clicked.timeoutId);
-        }
-      }
-      recentlyClicked.set(i, {
-        socketId: socket.id,
-        timeoutId: setTimeout(() => recentlyClicked.delete(i), 500),
-      });
-
-      // Handle the darn click
-      time('handleClick', () => {
-        const update = handleClick(i, rightClick);
-        if (update) {
-          io.emit('update', update);
-        }
-      });
-    }
-  });
-
-  socket.on('restart', () => restart() && io.emit('init', getPublicState()));
-
-  socket.on('disconnect', () => {
-    log(`user ${socket.id} disconnected`);
-    delete hovering[socket.id];
-    socket.broadcast.emit('hover', hovering);
-  });
-}
 
 // Displays logs on server startup
 function handleServerStartup() {
---
+++ server/socket.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const {getPublicState, restart, handleClick} = require('./minesweeper.js');
+const {log, time} = require('./logger.js');
+
+// Handles communication with a socket
+function handleConnection(socket, io) {
+  log(`user ${socket.id} connected`);
+  socket.emit('init', getPublicState());
+  socket.on('click', (data) => handleClickEvent(data, socket, io))
+      .on('hover', (i) => handleHover(i, socket))
+      .on('restart', () => restart() && io.emit('init', getPublicState()))
+      .on('disconnect', () => handleDisconnect(socket));
+}
+
+const recentlyClicked = new Map();
+function handleClickEvent([i, rightClick], socket, io) {
+  const state = getPublicState();
+  if (!state.gameInProgress) {
+    return;
+  }
+  // Avoid conflicting clicks from different clients
+  const clicked = recentlyClicked.get(i);
+  if (clicked) {
+    if (clicked.socketId !== socket.id) {
+      // Notify this client that the update failed
+      return socket.emit('update', {tiles: [[[i, state.board.tiles[i]]]]});
+    } else {
+      clearTimeout(clicked.timeoutId);
+    }
+  }
+  recentlyClicked.set(i, {
+    socketId: socket.id,
+    timeoutId: setTimeout(() => recentlyClicked.delete(i), 500),
+  });
+
+  // Handle the darn click
+  time('handleClick', () => {
+    const update = handleClick(i, rightClick);
+    if (update) {
+      io.emit('update', update);
+    }
+  });
+}
+
+const hovering = {};
+function handleHover(i, socket) {
+  if (i !== null) {
+    hovering[socket.id] = i;
+  } else {
+    delete hovering[socket.id];
+  }
+  socket.broadcast.emit('hover', hovering);
+}
+
+function handleDisconnect(socket) {
+  log(`user ${socket.id} disconnected`);
+  delete hovering[socket.id];
+  socket.broadcast.emit('hover', hovering);
+}
+
+exports.handleConnection = handleConnection;
